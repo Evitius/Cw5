d469e294ec692a16f7296d1bc75357a3e8b4eb83
Evitius <s18803@pjwstk.edu.pl>
2020-04-01 23:43:07 +0200

    Kod z wykładu

--- Cw5/Controllers/EnrollmentsController.cs
+++ Cw5/Controllers/EnrollmentsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using Cw5.DTOs.Requests;
 using Cw5.DTOs.Responses;
 using Cw5.Models;
@@ -18,9 +19,49 @@ namespace Cw5.Controllers
             {
                 return BadRequest("Zadanie jest niepoprawne");
             }
-
+         
             var st = new Student();
             st.FirstName = request.FirstName;
+            //....
+            //...
+
+            
+            using(var con = new SqlConnection(""))
+            using(var com= new SqlCommand())
+            {
+               
+                    com.Connection = con;
+                    con.Open();
+                    var tran = con.BeginTransaction();
+                try
+                {
+                    //1. Czy studia istnieją?
+                    com.CommandText = "select IdStudies from studies WHERE name=@name";
+                    com.Parameters.AddWithValue("name", request.Studies);
+
+                    var dr = com.ExecuteReader();
+                    if (!dr.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("Studia nie istnieją");
+                        //....
+                    }
+
+                    int idstudies = (int)dr["IdStudies"];
+
+                    //X. Dodanie studenta
+                    com.CommandText = "INSERT INTO Student(IndexNumber, FirstName) VALUES (@Index, @Fname)";
+                    com.Parameters.AddWithValue("index", request.IndexNumber);
+                    //...
+                    com.ExecuteNonQuery();
+                    tran.Commit();
+                }catch(SqlException exc)
+                {
+                    tran.Rollback();
+                }
+
+
+            }
 
             var response = new EnrollStudentResponse();
             response.LastName = st.LastName;
